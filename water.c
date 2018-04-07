a5ba43deec32bd6c13a486d4d53267f6e7f69c99
Carina Cruz <lecruz@bu.edu>
2018-04-07 03:44:51 +0000

    pseudo code water

---
+++ water.c
@@ -0,0 +1,16 @@
+// a program that prompts the user for the length of his or her shower in minutes (as a positive integer)
+// and then prints the equivalent number of bottles of water (as an integer)
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+
+int minutes = get_int();
+
+//step 1: prompt use to put in number of minutes
+//step 2: validate whether to the input is a number or alphabet
+//step 3: convert the number of minutes to bottles
+//step 4: print bottles
+
