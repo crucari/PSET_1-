66b78e07de0f70dc37d374f882155e7a90263f03
Carina Cruz <lecruz@bu.edu>
2018-04-09 04:12:28 +0000

    finished cash

--- cash.c
+++ cash.c
@@ -2,25 +2,83 @@
 
 // Steps:
 // 1) Prompt user for an amount of change
-    //   -the amount must make sense. We are using dollars. Use float
+//     -the amount must make sense. We are using dollars. Use float
 // 2)validate the input(aka making sure the input is an integer/a valid integer)
-        //      -make sure the given value is positive
-        //     -convert dollars to cents
-        //     -make sure to round
+//      -make sure the given value is positive
+//      -convert dollars to cents
+//     -make sure to round
 // 3) Make sur ehte largest coin possible is being used
 // 4) keep track of the coin being used
-        //  -how many coins to be returned
-        //  -amount to be returned
+//  -how many coins to be returned
+//  -amount to be returned
 // 5) print the final number of coins
 
 #include <stdio.h>
 #include <cs50.h>  //nessesary for us to use CS50 library function
+#include <math.h> //must include math library for round function
 int main(void)
 {
-    int n;
 
-    do
+    float input = -1;
+    int change = -1;
+
+    while (input < 0)
     {
 
-        n = get_int("Change owed:");
+        printf("Change owed:");
+        input = get_float();
+        if (input == 0)
+        {
+            exit(0);
+        }
+
     }
+
+    change = (int) roundf(input * 100);
+
+    int coins = 0;
+
+    while (change > 0)
+    {
+        if (change - 25 >= 0)
+        {
+            change = change - 25;
+            coins++;
+            continue;
+
+        }
+        else if (change - 10 >= 0)
+        {
+            change = change - 10;
+            coins++;
+            continue;
+
+        }
+        else if (change - 5 >= 0)
+        {
+            change = change - 5;
+            coins++;
+            continue;
+
+        }
+        else if (change - 1 >= 0)
+        {
+            change = change - 1;
+            coins++;
+            continue;
+
+        }
+
+        else
+
+        {
+            break;
+        }
+
+    }
+
+    printf("%i\n", coins);
+
+
+
+}
